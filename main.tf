f5f4879392b31aca716ee45eaca4cc1a0d83b742
LevelUpInTech <broadus@levelupintech.com>
2021-05-16 15:11:47 -0400

    add remote backend and var definitions

--- main.tf
+++ main.tf
@@ -69,3 +69,16 @@ resource "aws_s3_bucket_object" "app" {
 output "Endpoint" {
   value = aws_s3_bucket.app.website_endpoint
 }
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+  backend "s3" {
+    bucket = "bed919fd-ca91-1389-0356-94b922dc6744-backend"
+    key    = "terraform/webapp/terraform.tfstate"
+    region = "us-east-1"
+ }
+}
